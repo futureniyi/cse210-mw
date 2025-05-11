72b93089a28af8ad83e4e0990d6078b2564a8ee0
futureniyi <futureniyi@yahoo.com>
2025-05-11 14:13:23 +0100

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,79 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        float number = float.Parse(input);  // Convert string input to float
+
+        string letter = "";
+        string sign = "";
+        bool passedCourse = false;
+
+        // Determine letter grade
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Stretch Challenge 1: determine + or - sign
+        int remDigit = (int)number % 10;
+        if (remDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (remDigit < 3)
+        {
+            sign = "-";
+        }
+
+        // Stretch Challenge 2: No + for grades 93 and above
+        if (number >= 93)
+        {
+            sign = "";
+        }
+
+        // Stretch Challenge 3: No + or - if letter grade is F
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        // Print letter grade with sign
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        // Determine and print pass/fail message
+        if (number >= 70)
+        {
+            passedCourse = true;
+        }
+
+        if (passedCourse)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass. Keep trying and you’ll improve!");
+        }
     }
+
+
+
+
 }
